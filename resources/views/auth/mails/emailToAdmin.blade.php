b7d947633bfff850e455d1c01d8af6e4651e499f
Shik Vladimir <vnstore2018@gmail.com>
2022-01-10 12:02:38 +0300

    refactor|emailToUserConfirmation|send email to user confirmation(event)

---
+++ resources/views/auth/mails/emailToAdmin.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>email_to_user</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/templatemo.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <!--
+
+    TemplateMo 559 Priceman
+
+    https://templatemo.com/tm-559-Priceman
+
+    -->
+</head>
+<body>
+
+<p>Поступила новая заявка на регистрацию в Pricman service</p>
+<p>Для просмотра можете перейти по ссылке: http://laravel.work/admin/users</p>
+
+<h1 class="text-success logo h1 ">
+    Priceman - Admin
+</h1>
+
+</body>
+</html>
---
+++ resources/views/auth/mails/emailToUserConfirmation.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>email_to_user_confirmation</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Load fonts style after rendering the layout styles -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <!--
+
+    TemplateMo 559 Priceman
+
+    https://templatemo.com/tm-559-Priceman
+
+    -->
+</head>
+<body>
+
+<p>Доступ к сервису активирован!</p>
+<p>Войдите с использованием Вашего логина и пароля. Вход в систему: http://laravel.work/login</p>
+<p>Спасибо!</p>
+
+<h1 class="text-success logo h1 ">
+    Priceman
+</h1>
+
+</body>
+</html>
+
+
+
