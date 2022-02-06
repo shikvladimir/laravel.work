3d22bf5e396ca0e292da79df9d2bc39d019c2cbd
Shik Vladimir <vnstore2018@gmail.com>
2022-02-06 22:19:02 +0300

    refactor|Price_nereida|working on the comparison(sota success),(stt success),(nereida success),(serg is not working)

--- app/Helpers/ParsPrices/Price_nereida.php
+++ app/Helpers/ParsPrices/Price_nereida.php
@@ -4,6 +4,7 @@
 namespace App\Helpers\ParsPrices;
 
 
+use App\Models\Price;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class Price_nereida
@@ -13,6 +14,23 @@ class Price_nereida
         $getFileName = $_SERVER['DOCUMENT_ROOT'] . '/../prices_for_processing/price_nereida/Nereida_Price.xlsx';
         $spreadsheet = IOFactory::load($getFileName);
         $data = $spreadsheet->getSheet(0)->toArray();
-        dd($data);
+
+        $arrProduct = [];
+        for ($d = 0; $d < count($data); $d++) {
+            $article_price = $data[$d][4];
+            $availability = $data[$d][8];
+
+            if ($article_price != null) {
+                $article_db_product = Price::pluck('article')->toArray();
+                for ($i = 0; $i < count($article_db_product); $i++) {
+
+                    if ($article_price == $article_db_product[$i] && $availability != 'резерв') {
+                        $arrProduct[] = $article_db_product[$i];
+
+                    }
+                }
+            }
+        }
+        dd($arrProduct);
     }
 }
